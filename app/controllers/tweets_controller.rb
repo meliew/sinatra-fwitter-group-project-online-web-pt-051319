1952f5f909d7da1b06d9878a32b85a97b1b66763
Melissa Williams <melissa@brandmeetscopy.com>
2019-08-25 05:24:53 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,45 @@
 class TweetsController < ApplicationController
 
+  get '/tweets' do
+  if logged_in?
+    @tweets = Tweet.all
+    erb :"tweets/index"
+  else
+    redirect "/login"
+  end
+end
+
+  post '/tweets' do
+
+    @tweet = Tweet.create(content: params[:content])
+    @tweet.user_id = current_user.id
+    @tweet.save
+
+    redirect "/tweets"
+
+  end
+
+get 'tweets/new' do
+  if !logged_in?
+    redirect '/login'
+  else
+    erb :'/tweets/new'
+  end
+end
+
+get '/tweets/:id' do
+  if !logged_in
+    redirect '/login'
+  else
+    @tweet = Tweet.find_by_id(params[:id])
+    @user = User.find_by_id(params[:id])
+  erb :'tweets/show_tweet'
+  end
+end
+
+get '/tweets/:id/edit' do
+end
+
+
 
 end
