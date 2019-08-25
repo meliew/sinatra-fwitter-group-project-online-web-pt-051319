63ee221b5efe4accfa24d3947d568f292b0315b6
Melissa Williams <melissa@brandmeetscopy.com>
2019-08-25 21:46:59 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -41,6 +41,7 @@ class TweetsController < ApplicationController
   get '/tweets/:id/edit' do
     if logged_in?
       @tweet = Tweet.find_by_id(params[:id])
+      binding.pry
       if @tweet && @tweet.user == current_user
         erb :"/tweets/edit"
       else
