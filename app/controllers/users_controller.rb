5e6dd9b34518b4520f970b9a8eb940d6e3a829a4
Melissa Williams <melissa@brandmeetscopy.com>
2019-08-25 22:02:33 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,6 +34,7 @@ post '/login' do
   @user = User.find_by(:username => params[:username])
   if @user && @user.authenticate(params[:password])
     session[:user_id] = @user.id
+    binding.pry
     redirect '/tweets'
   else
     redirect '/login'
