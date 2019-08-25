d2f0b4952c5c35cbc916f29e09d4f3f16955b128
Melissa Williams <melissa@brandmeetscopy.com>
2019-08-25 20:44:57 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,7 +3,7 @@ class TweetsController < ApplicationController
   get '/tweets' do
   if logged_in?
     @tweets = Tweet.all
-    erb :"tweets/tweets"
+    erb :"tweets/index"
   else
     redirect "/login"
   end
